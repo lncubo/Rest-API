51eaa504e128f26a99579787fd12503bf899a39b
lncubo <79294724+lncubo@users.noreply.github.com>
2021-03-04 21:49:39 -0600

    Se hicieron cambios por que dejo de funcionar, solo se volvio a agregar las app en el book-api y quite codigo de new-book

--- book-api.js
+++ book-api.js
@@ -14,20 +14,22 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/book/:isbn', (req, res) => {
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+});
+
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+app.get('/book/:isbn', (req, res) => {
     // Reading isbn from the URL
     const isbn = req.params.isbn;
-    const newBook = req.body;
-
-    // Remove item from the books array
-    for (let i = 0; i < books.length; i++) {
-        let book = books[i]
-        if (book.isbn === isbn) {
-            books[i] = newBook;
-        }
-    }
-
-    res.send('Book is edited');
 });
 app.get('/book/:isbn', (req, res) => {
     // Reading isbn from the URL
@@ -58,5 +60,19 @@ app.delete('/book/:isbn', (req, res) => {
 
     res.send('Book is deleted');
 });
+app.post('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+    const newBook = req.body;
 
+    // Remove item from the books array
+    for (let i = 0; i < books.length; i++) {
+        let book = books[i]
+        if (book.isbn === isbn) {
+            books[i] = newBook;
+        }
+    }
+
+    res.send('Book is edited');
+});
 app.listen(port, () => console.log(`book app listening on port ${port}!`));
